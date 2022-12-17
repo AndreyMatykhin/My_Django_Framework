f3db0dc2fdc2ff4fc28b7d62c784431d7f8e6f1a
MAA <matykhinand2021@gmail.com>
2022-12-18 01:50:38 +0300

    task 4 compleate

--- my_braniaclms/mainapp/views/contacts.py
+++ my_braniaclms/mainapp/views/contacts.py
@@ -1,7 +1,42 @@
-__all__ = ['ContactsView']
-
+from django.contrib import messages
+from django.core.cache import cache
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
+from mainapp import forms as mainapp_forms
+from mainapp import tasks as mainapp_tasks
 
 
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactsView, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context["form"] = mainapp_forms.MailFeedbackForm(user=self.request.user)
+        return context
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            cache_lock_flag = cache.get(f"mail_feedback_lock_{self.request.user.pk}")
+            if not cache_lock_flag:
+                cache.set(
+                    f"mail_feedback_lock_{self.request.user.pk}",
+                    "lock",
+                    timeout=300,
+                )
+                messages.add_message(self.request, messages.INFO, _("Message sended"))
+                mainapp_tasks.send_feedback_mail.delay(
+                    {
+                        "user_id": self.request.POST.get("user_id"),
+                        "message": self.request.POST.get("message"),
+                    }
+                )
+            else:
+                messages.add_message(
+                    self.request,
+                    messages.WARNING,
+                    _("You can send only one message per 5 minutes"),
+                )
+        return HttpResponseRedirect(reverse_lazy("mainapp:contacts"))
