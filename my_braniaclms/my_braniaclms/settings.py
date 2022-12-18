48f51d6d3660810832bc36cd2c4e6bfcec0e56d2
MAA <matykhinand2021@gmail.com>
2022-12-18 21:24:25 +0300

    task 2 compleate

--- my_braniaclms/my_braniaclms/settings.py
+++ my_braniaclms/my_braniaclms/settings.py
@@ -59,6 +59,7 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -208,3 +209,5 @@ CELERY_BROKER_URL = "redis://localhost:6379"
 CELERY_RESULT_BACKEND = "redis://localhost:6379"
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = "var/email-messages/"
+
+LOCALE_PATHS = [BASE_DIR / "locale"]
