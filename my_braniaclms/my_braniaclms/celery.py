4bcc78eb5aec14302089fd2f58bb87e6372cc2d1
MAA <matykhinand2021@gmail.com>
2022-12-22 07:44:50 +0300

    task 1 compleate

--- my_braniaclms/my_braniaclms/celery.py
+++ my_braniaclms/my_braniaclms/celery.py
@@ -2,7 +2,10 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_braniaclms.settings")
+import my_braniaclms.settings
+
+if my_braniaclms.settings.DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_braniaclms.settings")
 
 celery_app = Celery("braniac")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
